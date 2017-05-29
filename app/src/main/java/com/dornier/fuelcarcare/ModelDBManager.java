0403c41d894dafed4e7d8fb6232a31bcc7cb15ca
Dornier <ronan-mtba@hotmail.com>
2017-05-29 18:44:56 -0300

    updated

---
+++ app/src/main/java/com/dornier/fuelcarcare/ModelDBManager.java
@@ -0,0 +1,114 @@
+package com.dornier.fuelcarcare;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+/**
+ * Created by Dornier on 29/05/2017.
+ */
+
+public class ModelDBManager extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "local_db.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public ModelDBManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(database_create_fill_ups);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    /**Tabela fill_ups**/
+
+    public static final String table_name_fill_ups =    "fill_ups";
+    public static final String fill_ups_id =            "_id";
+    public static final String fill_ups_final_price =   "final_price";
+    public static final String fill_ups_fuel_price =    "fuel_price";
+    public static final String fill_ups_fuel_amount =   "fuel_amount";
+    public static final String fill_ups_location =      "location";
+    public static final String fill_ups_fill_date =     "fill_date";
+    public static final String fill_ups_fuel =          "fuel";
+    public static final String fill_ups_car_id =        "car_id";
+    public static final String fill_ups_status =        "status";
+
+    private static final String database_create_fill_ups =  "create table "
+            + table_name_fill_ups + "( " +
+            fill_ups_id + " integer primary key autoincrement, " +
+            fill_ups_final_price    +" text not null, " +
+            fill_ups_fuel_price     +" text not null, " +
+            fill_ups_fuel_amount    +" text not null, " +
+            fill_ups_location       +" text not null, " +
+            fill_ups_fill_date      +" text not null, " +
+            fill_ups_fuel           +" text not null, " +
+            fill_ups_car_id         +" text not null, " +
+            fill_ups_status         +" text not null\" +);";
+
+    /**Tabela expenses**/
+
+    public static final String table_name_expenses =    "expenses";
+    public static final String expenses_id =            "_id";
+    public static final String expenses_price =         "price";
+    public static final String expenses_quantity =      "quantity";
+    public static final String expenses_component_name ="component_name";
+    public static final String expenses_expense_date =  "expense_date";
+    public static final String expenses_car_id =        "car_id";
+    public static final String expenses_status =        "status";
+
+    private static final String database_create_expenses =  "create table "
+            + table_name_expenses + "( " +
+            expenses_id + " integer primary key autoincrement, " +
+            expenses_price          +" text not null, " +
+            expenses_quantity       +" text not null, " +
+            expenses_component_name +" text not null, " +
+            expenses_expense_date   +" text not null, " +
+            expenses_car_id         +" text not null, " +
+            expenses_status         +" text not null\" +);";
+
+    /**tabela maintenances**/
+
+    public static final String table_name_maintenances =        "maintenances";
+    public static final String maintenances_id =                "_id";
+    public static final String maintenances_kilometers =        "kilometers";
+    public static final String maintenances_item =              "item";
+    public static final String maintenances_maintenance_date =  "maintenance_date";
+    public static final String maintenances_car_id =            "car_id";
+    public static final String maintenances_status =            "status";
+
+    private static final String database_create_maintenances =  "create table "
+            + table_name_maintenances + "( " +
+            maintenances_id + " integer primary key autoincrement, " +
+            maintenances_kilometers         +" text not null, " +
+            maintenances_item               +" text not null, " +
+            maintenances_maintenance_date   +" text not null, " +
+            maintenances_car_id             +" text not null, " +
+            maintenances_status             +" text not null\" +);";
+
+    /**tabela vehicles**/
+
+    public static final String table_name_vehicles =    "vehicles";
+    public static final String vehicles_id =            "_id";
+    public static final String vehicles_name =          "name";
+    public static final String vehicles_manufacturer =  "manufacturer";
+    public static final String vehicles_model =         "model";
+    public static final String vehicles_year =          "year";
+    public static final String vehicles_status =        "status";
+
+    private static final String database_create_vehicles =  "create table "
+            + table_name_vehicles + "( " +
+            vehicles_id + " integer primary key autoincrement, " +
+            vehicles_name           +" text not null, " +
+            vehicles_manufacturer   +" text not null, " +
+            vehicles_model          +" text not null, " +
+            vehicles_year           +" text not null, " +
+            vehicles_status         +" text not null\" +);";
+}
